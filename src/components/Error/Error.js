2acbb54322a4f281a810daf7ef014bb92aa8636f
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-03 00:59:28 +0300

    Added basic custom styles to the page, customized error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Error = props => {
   if (props.hasError) {
     return (
-      <div>Sorry, we couldn`t find news for you. Please, try again later</div>
+      <div className="error error_msg">
+        Sorry, we couldn`t find news for you. Please, try again later
+      </div>
     );
   }
   return props.children;
