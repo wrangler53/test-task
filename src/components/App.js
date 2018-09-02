749f63880e561133d0aa2d61f41c6a155e8928cd
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-02 21:05:09 +0300

    Render news list on page in accordion

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'react-materialize';
+import { getTenLastNews } from '../api/api';
 
+import NewsList from './NewsList/NewsList';
 class App extends Component {
+  state = {
+    news: [],
+    isError: false
+  }
+
+  componentDidMount() {
+    this.loadTenLastNews();
+  }
+
+  loadTenLastNews() {
+    getTenLastNews()
+      .then(news => this.setState({ news }))
+      .catch(() => this.setState({ isError: true }));
+  }
+
   render() {
     return (
       <div className="container">
-
+        <h1>The Guardian News</h1>
+        <Button waves='light' onClick={() => this.loadTenLastNews()}>
+          Refresh
+          <Icon right>refresh</Icon>
+        </Button>
+        <NewsList
+          news={this.state.news}
+          error={this.state.isError}
+        /> {/*TODO: check could I set in props all state*/}
       </div>
     );
   }
---
+++ src/components/NewsList/NewsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+
+const NewsList = ({ news }) => (
+  <Collapsible accordion defaultActiveKey={1}>
+    {
+      news.map(item =>
+        <CollapsibleItem header={item.webTitle}>
+          Lorem ipsum dolor sit amet.
+        </CollapsibleItem>
+      )
+    }
+  </Collapsible>
+);
+
+export default NewsList;
